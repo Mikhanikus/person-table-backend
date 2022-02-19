91c184050b31e915d224502ebbd5da52e99326de
Mikhail Iontsev <mihanikus234@gmail.com>
2022-02-19 08:31:44 +0300

    initial commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 const {db} = require("../database/db");
 var router = express.Router();
+var cors = require('cors')
 
+var allowlist = ['https://person-table-test.herokuapp.com']
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (allowlist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true,  methods: ['GET', 'PUT', 'POST', "DELETE"]} // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
+}
 
-router.get('/api', async function(req, res) {
+router.get('/api', cors(corsOptionsDelegate), async function(req, res) {
   if (Object.keys(req.body).length === 0) {
     db(`SELECT * FROM person;
 `).then((value) => res.send(value))
@@ -14,13 +25,13 @@ router.get('/api', async function(req, res) {
   }
 
 });/**/
-router.post('/api', function(req, res) {
+router.post('/api', cors(corsOptionsDelegate), function(req, res) {
   // /*res.render('index', { title: 'Express' });*//*/?*/INSERT INTO table_name(column1, column2, …)
   db(`INSERT INTO "person"(name, age, education, occupation) VALUES('${req.body.name}', '${req.body.age}', '${req.body.education}', '${req.body.occupation}') RETURNING *;
 `).then((value) => res.send(value))
 
 });/**/
-router.put('/api', function(req, res) {
+router.put('/api', cors(corsOptionsDelegate), function(req, res) {
   db(` UPDATE "person"
   SET name = '${req.body.name}',
       age = '${req.body.age}',
@@ -32,7 +43,7 @@ router.put('/api', function(req, res) {
 
 
 /* GET home page. */
-router.delete('/api', function(req, res) {
+router.delete('/api', cors(corsOptionsDelegate), function(req, res) {
   // /*res.render('index', { title: 'Express' });*//*/?*/
   db(`DELETE FROM person WHERE id=${req.body.id} RETURNING *;
 `).then(() => res.send("success"))
